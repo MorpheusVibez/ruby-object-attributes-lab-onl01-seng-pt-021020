2c15de3eccf6b9869c70a648b49efb44019626f4
Andre Skeene <halodre24@gmail.com>
2020-02-23 23:39:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 class Dog
-  def name=(name)
+  def self.name(name)
     @name = name
   end
 
   def name
     @name
+  end
+
+  def self.breed(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
   end
 end
